88a917722627b4bca45e23dbdaf8262c6bbe8865
Christopher-Eunho <christopher.eunho@gmail.com>
2022-03-26 12:28:24 -0700

    initial

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ const logger = morgan("dev"); // "dev" is a config option
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views") // change the location of views from default
 app.use(logger); //Global middleware
-app.use(express.urlencoded({extended : true})); // parses html form and translate into JS object
+~
 
 
 /* 
