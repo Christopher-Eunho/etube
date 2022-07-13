08091c8557873885f736fd447a7a199811247d92
Christopher-Eunho <christopher.eunho@gmail.com>
2022-07-13 13:25:49 -0700

    Player setup using HTML DOM (Media Element). Implemented play/pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,29 @@
-console.log("Video Player");
+/*
+Create / Modify video player using the HTML DOM API(HTMLMediaElement Interface)
+ ** https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
+*/
+
+const video = document.querySelector("video"); // The html Video object 
+
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
