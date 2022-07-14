fe45e0ddb1b95ef8ae8381df74fe2ea58c1cfa23
Christopher-Eunho <christopher.eunho@gmail.com>
2022-07-13 22:03:27 -0700

    play, pause mute basics set up

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -8,22 +8,56 @@ const video = document.querySelector("video"); // The html Video object
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
-  if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+
+    playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
+const handleMuteClick = (e) => {
+    if (video.muted) {
+        video.muted = false;
+        video.volume = 0.5;
+    } else {
+        video.muted = true;
+        video.volume = 0;
+    }
+        muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+        volumeRange.value = video.muted ? 0 : 0.5;
+    };
+  
+const handleVolumeChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    if (video.muted) {
+      video.muted = false;
+      muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = value;
 
-const handleMute = (e) => {};
+    if (Number(value) === 0) {
+        muteBtn.innerText = "Unmute";
+        video.muted = true;
+    } else {
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+  };
+
+
 
 playBtn.addEventListener("click", handlePlayClick);
-muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
+muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeChange);
+
