990f44f6d3eb41e89a928058b33302e33e7b644d
Christopher-Eunho <christopher.eunho@gmail.com>
2022-07-25 11:58:13 -0700

    Media recording preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -6,14 +6,15 @@ https://developer.mozilla.org/en-US/docs/Web/API/Media_Streams_API
 */
 
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: true,
+    video: false,
   });
-  console.log(stream);
-  console.log("sds");
+  video.srcObject = stream;
+  video.play();
 };
 
 startBtn.addEventListener("click", handleStart);
