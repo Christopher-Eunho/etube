945ad0aa7ff88d5253de6f76a2dfe3fe3c3cd8d3
Christopher-Eunho <christopher.eunho@gmail.com>
2022-07-26 11:33:37 -0700

    Added Recording start button. Media recording setup

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,18 +3,55 @@ Handle video recording in upload page
 Mainly use Media Capture and Streams API
 https://developer.mozilla.org/en-US/docs/Web/API/Media_Streams_API
 
+Media Recorder:
+https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder
+
+dataavailable (event)
+Fires periodically each time timeslice milliseconds of media have been recorded 
+(or when the entire media has been recorded, if timeslice wasn't specified). 
+The event, of type BlobEvent, contains the recorded media in its data property.
 */
 
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
-    video: false,
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(stream);
+  
+  recorder.ondataavailable = (e) => {
+    console.log("recording done");
+    console.log(e);
+    console.log(e.data);
+  };
+  console.log(recorder);
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 4000);
+};
+
+// get video stream and show preview 
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true,
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
