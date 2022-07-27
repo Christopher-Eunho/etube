1889eb2a2bc0c9be1e858b7e0781cc90b532af20
Christopher-Eunho <christopher.eunho@gmail.com>
2022-07-27 12:25:15 -0700

    Video recording and downloading featrure

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -17,8 +17,17 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
 
-const handleDownload = () => {};
+
+// Create a downloadable link and add it to the body of document. Then simulate click of the link 
+const handleDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "EtubeRecording.webm"; // name of the video file
+  document.body.appendChild(a);
+  a.click();
+};
 
 const handleStop = () => {
   startBtn.innerText = "Download Recording";
@@ -32,11 +41,12 @@ const handleStart = () => {
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
 
-  recorder = new MediaRecorder(stream);
+  // specify the type of the video as webm so it's available on many browsers. (mp4 doesn't work on some browsers)
+  recorder = new MediaRecorder(stream, {mimeType: "video/webm"}); 
   // Fires when data is available (when recording stops)
   recorder.ondataavailable = (event) => {
     // Creates a video url that is saved on the browser 
-    const videoFile = URL.createObjectURL(event.data); 
+    videoFile = URL.createObjectURL(event.data); 
     // Replace the preview video with the recorded video
     video.srcObject = null;
     video.src = videoFile;
